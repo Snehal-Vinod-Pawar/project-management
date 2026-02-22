84e6e92da4067345cf2f3c398df2baa3f0be1227
Snehal Pawar <snehalpawar2005@gmail.com>
2026-02-23 00:47:03 +0530

    final cookie fix remove domain

--- server/src/utils/cookies.ts
+++ server/src/utils/cookies.ts
@@ -1,13 +1,11 @@
 import { Response } from "express";
 
 export const setAuthCookie = (res: Response, token: string) => {
-
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "none", 
+    sameSite: "none",
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    domain: ".onrender.com",
   });
 };
