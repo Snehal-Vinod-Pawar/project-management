93816414070374888544e81ef3debe2b21f33c16
Snehal Pawar <snehalpawar2005@gmail.com>
2026-02-22 18:32:32 +0530

    fix login cookie

--- server/src/utils/cookies.ts
+++ server/src/utils/cookies.ts
@@ -1,12 +1,11 @@
 import { Response } from "express";
 
 export const setAuthCookie = (res: Response, token: string) => {
-  const isProd = process.env.NODE_ENV === "production";
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: isProd,                 // true only on render
-    sameSite: isProd ? "none" : "lax", 
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
