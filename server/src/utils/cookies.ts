050d1a633242e1df0ab5199bf4912ee7349ef335
Snehal Pawar <snehalpawar2005@gmail.com>
2026-02-22 18:44:56 +0530

    final cookie fix

--- server/src/utils/cookies.ts
+++ server/src/utils/cookies.ts
@@ -4,8 +4,8 @@ export const setAuthCookie = (res: Response, token: string) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
+    secure: false,
+    sameSite: "lax", 
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
