32b189f4e13a39f810c6eccb1ecfca16f76b35c7
Snehal Pawar <snehalpawar2005@gmail.com>
2026-03-02 14:27:55 +0530

    Fix multer type issue for Render build

--- server/src/middleware/upload.ts
+++ server/src/middleware/upload.ts
@@ -1,12 +1,25 @@
-import multer, { FileFilterCallback } from "multer";
+import multer from "multer";
 import { Request } from "express";
 
 const storage = multer.diskStorage({
   destination: "public/uploads/tasks",
-  filename: (req: Request, file: Express.Multer.File, cb) => {
+  filename: (req: Request, file: any, cb) => {  
     const uniqueName = Date.now() + "-" + file.originalname;
     cb(null, uniqueName);
   },
 });
 
 export const uploadTaskImage = multer({ storage });
+
+// import multer, { FileFilterCallback } from "multer";
+// import { Request } from "express";
+
+// const storage = multer.diskStorage({
+//   destination: "public/uploads/tasks",
+//   filename: (req: Request, file: Express.Multer.File, cb) => {
+//     const uniqueName = Date.now() + "-" + file.originalname;
+//     cb(null, uniqueName);
+//   },
+// });
+
+// export const uploadTaskImage = multer({ storage });
