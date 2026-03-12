88a28b7ee44938b3cd264d7efea1a7a8d463ff5c
Snehal Pawar <snehalpawar2005@gmail.com>
2026-03-12 20:40:07 +0530

    force production api url

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:8000",
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true, 
 });
