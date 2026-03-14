3acbec4f8f41b70e7dc46ad812db7ddc37b916ea
Snehal Pawar <snehalpawar2005@gmail.com>
2026-03-15 00:59:34 +0530

    fix multer upload field

--- server/src/routes/taskRoutes.ts
+++ server/src/routes/taskRoutes.ts
@@ -13,7 +13,8 @@ import { uploadTaskImage } from "../middleware/upload";
 const router = Router();
 
 router.get("/",protect,getTasks);
-router.post("/",protect,uploadTaskImage.single("image"),createTask);
+// router.post("/",protect,uploadTaskImage.single("image"),createTask);
+router.post("/", protect, uploadTaskImage.single("file"), createTask);
 router.patch("/:taskId/status", protect,updateTaskStatus);
 router.get("/user/:userId", protect,getUserTasks);
 router.get("/priority/:priority", protect, getTasksByPriority);
