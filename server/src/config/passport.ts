954fd36bfc3b8386912281e9075a4e016c6ccf68
Snehal Pawar <snehalpawar2005@gmail.com>
2026-08-18 22:12:19 +0530

    fix: configure production database migrations

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -9,7 +9,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            callbackURL: "http://localhost:8000/auth/google/callback",
+            callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
         },
         async (
             accessToken: string,
@@ -46,7 +46,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID!,
             clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-            callbackURL: "http://localhost:8000/auth/github/callback",
+            callbackURL: `${process.env.BASE_URL}/auth/github/callback`,
             scope: ["user:email"],
         },
         async (
